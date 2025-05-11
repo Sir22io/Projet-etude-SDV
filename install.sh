23901e30801f3e948ba59b818b285e82636f9d3c
Soso951 <sofianekhodja95100@gmail.com>
2025-05-11 17:34:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,23 @@
 echo "Installation des outils nécessaires..."
 
 # Update package list
- apt-get update
+ sudo apt-get update
 
 # Installer les outils
- apt-get install -y python3 python3-pip masscan nikto gobuster nmap commix binwalk rkhunter chkrootkit
+ sudo apt-get install -y python3 python3-pip masscan nikto gobuster nmap commix binwalk rkhunter chkrootkit
 
 # Installer SQLMap
 if ! command -v sqlmap &> /dev/null
 then
     echo "Installation de SQLMap..."
-     apt-get install -y sqlmap
+    sudo apt-get install -y sqlmap
 fi
 
 # Installer WPScan (via Ruby)
 if ! command -v wpscan &> /dev/null
 then
     echo "Installation de WPScan..."
-     apt-get install ruby-full
+     sudo apt-get install ruby-full
      gem install wpscan
 fi
 
@@ -27,35 +27,35 @@ fi
 if ! command -v gvm &> /dev/null
 then
     echo "Installation d'OpenVAS..."
-     apt-get install -y openvas
+     sudo apt-get install -y openvas
 fi
 
 # Installer Suricata
 if ! command -v suricata &> /dev/null
 then
     echo "Installation de Suricata..."
-     apt-get install -y suricata
+     sudo apt-get install -y suricata
 fi
 
 # Installer Arachni
 if ! command -v arachni &> /dev/null
 then
     echo "Installation d'Arachni..."
-     apt-get install -y arachni
+     sudo apt-get install -y arachni
 fi
 
 # Installer Fimap
 if ! command -v fimap &> /dev/null
 then
     echo "Installation de Fimap..."
-     apt-get install -y fimap
+     sudo apt-get install -y fimap
 fi
 
 # Installer Clusterd
 if ! command -v clusterd &> /dev/null
 then
     echo "Installation de Clusterd..."
-     apt-get install -y clusterd
+     sudo apt-get install -y clusterd
 fi
 
 # Installation des packages Python requis
